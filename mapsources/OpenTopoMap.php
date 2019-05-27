7fa7c4fe7d734aa4d0cfc9214afb31043babc461
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2019-05-27 11:58:53 +0300

    impr source

--- mapsources/OpenTopoMap.php
+++ mapsources/OpenTopoMap.php
@@ -12,13 +12,26 @@ $trash = array(
 
 $functionGetURL = <<<'EOFU'
 function getURL($z,$x,$y) {
-/* Алгоритм получения ссылки на тайл заимствован из SAS.Planet
+/* К сожалению, OpenTopoMap очень не приветствует массовое скачивание карты, следит за этим,
+и банит по ip. Бан заключается в тридцатисекундной задержке отдачи тайла. Также, возможно,
+случайные тайлы не отдаются совсем, с ответом 404.
+Это всё не препятствует обычному просмотру карты, но, если надо скачать более-менее обширные
+площади - скачивать надо, достаточно часто меняя ip. Проще всего это сделать через tor.
+Приведённая сдесь конфигурация предполагает, что на этой же машине имеется узел tor
+и proxy lightdm, сконфигурированный только для приёма http и передаче их tor'у по socs.
+У tor должен быть включен управляющий сокет.
+Если клиент умеет сессии - то команда tor'у сменить выходную ноду передаётся через несколько
+тайлов, если нет (загрузчик в cli, например) - выходная нода меняется каждый тайл.
+Разумеется, смена выходной ноды не гарантирует смену ip, но в среднем всё работает.
+
+По умолчанию всё это отключено. Для включения нужно раскомментировать параметр 'proxy' в массиве $opts
+
  http://192.168.10.10/tileproxy/tiles.php?z=12&x=2374&y=1161&r=OpenTopoMap
 */
 //error_log("OpenTopoMap $z,$x,$y");
 $server = array('a','b','c');
 $getTorNewNode = "(echo authenticate '\"\"'; echo signal newnym; echo quit) | nc localhost 9051"; 	// set it if you hawe Tor as proxy, and want change exit node every $tilesPerNode try. https://stackoverflow.com/questions/1969958/how-to-change-the-tor-exit-node-programmatically-to-get-a-new-ip
-$tilesPerNode = 10;
+$tilesPerNode = 10; 	// попытка смены ip предпринимается каждые столько тайлов
 
 $userAgents = array();
 $userAgents[] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36';
