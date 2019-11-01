04e5427981951d4db08d8c6798a6a91c0330ec0c
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2019-11-01 20:31:58 +0300

    loader run indicator

--- loaderSched.php
+++ loaderSched.php
@@ -58,12 +58,14 @@ do {
 	}
 	// Всё, я работаю
 	// Занесём себя в cron
+	
 	if(! $inCron) { 	// стараемся, чтобы в cron была только одна запись о запуске нас. Хотя это не должно плохо кончаться
 		// удалим себя из cron, потому что я мог быть запущен cron'ом, а умерший - не мог удалить
 		exec("crontab -l | grep -v '$fullSelfName'  | crontab -");
 		exec('(crontab -l ; echo "* * * * * '.$phpCLIexec.' '.$fullSelfName.'") | crontab -'); 	// каждую минуту
 		$inCron = TRUE;
 	}
+	
 	// Проверим наличие, установим и снимем задания
 	foreach($jobs as $i => $job) {
 		//echo "Очередь заданий к началу обработки:"; print_r($jobs); echo "\n";
@@ -166,6 +168,7 @@ do {
 			//exec("$phpCLIexec loader.php > /dev/null &");
 		}
 	}
+//LOOP:
 	//echo "runs=$runs; nextJob=$nextJob;\n";
 	sleep(5);
 } while($jobs);
