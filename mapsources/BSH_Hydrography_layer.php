fd6840093072452aad77ef3b59377f32eeb3b03d
Vladimir Kalachikhin <v.kalachikhin@gmail.com>
2021-01-17 18:21:50 +0300

    upd

--- mapsources/BSH_Hydrography_layer.php
+++ mapsources/BSH_Hydrography_layer.php
@@ -5,6 +5,7 @@ require('BSH_common');
 $getURLparams = array(
 'map'=>'NAUTHIS_Hydrography',
 'layers'=>'29,30,65,66,101,102,137,138,173,174,209,210',
+//'bgcolor'=>'0xFFFFFF' 
 'bgcolor'=>'0x777777' 
 );
 
